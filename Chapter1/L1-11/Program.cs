f1f7e2dae0c87b399789808cf9116ffc2a6ad664
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-09-29 21:48:39 +0330

    add: L11

---
+++ Chapter1/L1-11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace L1_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            },TaskContinuationOptions.OnlyOnCanceled);
+
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+          var completedTask=  t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+          
+            completedTask.Wait();
+        }
+    }
+}
