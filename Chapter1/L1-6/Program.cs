5a6fed28fa3b7890c55a788f7e6daa1a28ba7d35
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-09-29 21:27:53 +0330

    add : L-6 and L-7

---
+++ Chapter1/L1-6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace L1_6
+{
+    internal class Program
+    {
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
+        {
+            
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Theard A: {0}", i);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Theard B: {0}", i);
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter1/L1-7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace L1_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on Thread from Threadpool");
+            });
+            Console.ReadLine();
+        }
+    }
+}
