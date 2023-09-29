66d432f2ebd96afba80576e27a38f0020de87fca
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-09-29 22:42:53 +0330

    add: L1-13

---
+++ Chapter1/L1-13/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace L1_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var result = new Int32[3];
+                TaskFactory tf= new TaskFactory(TaskCreationOptions.AttachedToParent,TaskContinuationOptions.ExecuteSynchronously);
+
+                tf.StartNew(() => result[0] = 0);
+                tf.StartNew(() => result[1] = 1);
+                tf.StartNew(() => result[2] = 2);
+                return result;
+            });
+            var finalTask = parent.ContinueWith(parentTask =>
+            {
+                foreach (var i in parentTask.Result)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+            finalTask.Wait();
+        }
+    }
+}
