f80067b2b7af197688866f036b29c0fcaf7ecfd5
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-09-29 17:45:30 +0330

    add chapter

--- Chapter1/L1-1/Program.cs
+++ Chapter1/L1-1/Program.cs
@@ -1,25 +1,27 @@
-﻿namespace Chapter1
+﻿namespace L1_1
 {
     internal class Program
     {
         public static void ThreadMethod()
         {
-            for (int i = 0; i < 10; i++) { 
-            Console.WriteLine("ThreadProc : {0}",i);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc : {0}", i);
                 Thread.Sleep(0);
             }
         }
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello, World!");
-            Thread t =new Thread(new ThreadStart( ThreadMethod));
+            
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
             t.Start();
-            for (int i = 0;i < 4;i++)
+            for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Main thread: Do Some Work.");
-                Thread.Sleep(2);
+                Thread.Sleep(0);
             }
             t.Join();
+             
         }
     }
 }
---
+++ Chapter1/L1-2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L1_2
+{
+    internal class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc : {0}", i);
+                Thread.Sleep(10000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t=new Thread(new ThreadStart( ThreadMethod));
+            t.IsBackground = true;
+            t.Start();
+        }
+    }
+}
