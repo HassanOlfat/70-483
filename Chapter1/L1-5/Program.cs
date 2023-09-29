967abb1b9cf9d14f6489f2915e4afe49710def8c
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-09-29 18:06:03 +0330

    add: L1_5

---
+++ Chapter1/L1-5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace L1_5
+{
+    internal class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("thread A:{0}", _field);
+                }
+            }).Start();
+           
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("thread B:{0}", _field); 
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
