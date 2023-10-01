7c5d9e3bdf8e347d0977ec07a33cbc38cb7d950e
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-01 21:09:03 +0330

    add: L1-16

---
+++ Chapter1/3_Parallel/L1-16/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace L1_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i => { Thread.Sleep(1000); });
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers,i=> { Thread.Sleep(1000); });
+
+        }
+    }
+}
