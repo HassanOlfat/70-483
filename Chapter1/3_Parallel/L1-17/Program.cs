2cc601ea8b2805ef044538be54ad8b6b8a25d53a
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-01 21:13:42 +0330

    add: L1_17

---
+++ Chapter1/3_Parallel/L1-17/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace L1_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 500) {
+                    Console.WriteLine("Breaking Loop");
+                    loopState.Break();
+                }
+                return;
+            });
+        }
+    }
+}
