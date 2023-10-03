b7122e0c9de13122e8078c0ee950c036083ee983
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-03 16:30:30 +0330

    add: L-19

---
+++ 4-Async_Await/L1-19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace L1_19
+{
+    internal class Program
+    {
+        static   void Main(string[] args)
+        {
+          
+        }
+        public static Task SleepAsyncA(int millisecondesTimeout)
+        {
+            return Task.Run(() => { Thread.Sleep(millisecondesTimeout); });
+        }
+        public static Task SleepAsyncB(int millisecondesTimeout)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs= new TaskCompletionSource<bool>();
+            t.Change(millisecondesTimeout, -1);
+
+            return tcs.Task;
+        }
+    }
+}
