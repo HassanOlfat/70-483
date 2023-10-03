abb3e1b102578093b60f4a51d017646392116eb5
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-03 15:56:50 +0330

    add: L1-18

---
+++ 4-Async_Await/L1-18/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L1_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        private static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("https://hassanolfat.ir");
+                return result;
+            }
+        }
+    }
+}
