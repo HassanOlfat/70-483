e7ac308836a79355125efd74ef8270d4d4816cd8
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-03 22:14:19 +0330

    Add: L1-22

---
+++ 5-PLINQ/L1-22/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace L1_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 100000000);
+            var parallelResult = numbers.AsParallel()
+                .Where(x => x %2== 0)
+                .ToArray();
+        }
+    }
+}
---
+++ 5-PLINQ/L1-23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L1_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
