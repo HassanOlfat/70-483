d0f53dbc14af4f9c5b505eaabbd4d8b5169fbc25
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-03 22:19:16 +0330

    add: L1-23

--- 5-PLINQ/L1-23/Program.cs
+++ 5-PLINQ/L1-23/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = Enumerable.Range(0, 100000000);
+            var parallelResult = numbers.AsParallel()
+                .Where(x => x % 2 == 0)
+                .ToArray();
+
+          
+            foreach (var result in parallelResult) { Console.WriteLine(result); }
+            
         }
     }
 }
