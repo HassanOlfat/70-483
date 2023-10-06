8add7f2f2d6c40db31b7c1a7adc0e166c6117635
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2023-10-06 17:56:31 +0330

    add: L1-27

---
+++ 5-PLINQ/L1-27/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace L1_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           var numbers = Enumerable.Range(0, 20);
+            try
+            {
+                var parallelResult = numbers.AsParallel()
+                    .Where(i => IsEven(i));
+
+                parallelResult.ForAll(e => Console.WriteLine(e));
+            }
+            catch (AggregateException exception)
+            {
+
+                Console.WriteLine("There where {0} exceptions",exception.InnerExceptions.Count);
+            }
+        }
+
+        private static bool IsEven(int i)
+        {
+            if (i % 10 == 0) throw new AggregateException("i");
+            return i % 2 == 0;
+        }
+    }
+}
